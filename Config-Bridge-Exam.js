6a8ec144b2f11d14a515b75018bcc31c65f96f4f
alsys123 <alsys123@gmail.com>
2025-12-05 14:13:26 -0500

    Create Config-Bridge-Exam.js

---
+++ Config-Bridge-Exam.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+        apiKey: 'AIzaSyD9hYZTf-8u-mgnwz1JsjU44UJ6DurbXdc',
+        //     spreadsheetId: '1M4fAtFnLxhz7i-94RjwofLJh7f0xrxVinjbjz9X2b9A',  // CIXExam
+        spreadsheetId: '1zq2h3RbAtpy3N7NUa2VcjYzCDRQqG8UsUcuIRdzZMSI',  // bridgeExam
+        questionsRange: ['examQuestions!A:J', 'sectionPartTitles!A:E'],
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
+      };
